6a1adb3f9d02a6b87cc0dc3dfc7df6ffa3599c44
Thomas Rene Andresen <thomas.r.andresen@hotmail.no>
2015-01-12 23:06:04 +0100

    New travel view

--- views/jsx/DashboardView.jsx
+++ views/jsx/DashboardView.jsx
@@ -6,33 +6,45 @@ var NewsView = require("../../news/NewsView");
 module.exports = function(data) {
 	return (
 		<div className="dashboard">
-			<div className="quadrant">
-				<ClockView { ...data.clockProps }/>
-			</div>
-			<div className="quadrant travel">
-			  <div className="list">
-				<div className="row group-header">T-BANE</div>
-				<div className="row header">VEST</div>
-				<div className="group west"></div>
-				<div className="row header">ØST</div>
-				<div className="group east"></div>
-			  </div>
-			</div>
-			<div className="quadrant">
-			  <div className="octant weather">
-					<WeatherView { ...data.weatherProps } />
-			  </div>
-			  <div className="octant calendar">
-				<div className="list">
-				  <div className="row header">I DAG</div>
-				  <div className="group today"></div>
-				  <div className="row header">I MORGEN</div>
-				  <div className="group tomorrow"></div>
+			<div className="main">
+				<div className="quadrant">
+					<ClockView { ...data.clockProps }/>
 				</div>
-			  </div>
+				<div className="quadrant travel">
+				</div>
+				<div className="quadrant">
+				  <div className="octant weather">
+						<WeatherView { ...data.weatherProps } />
+				  </div>
+				  <div className="octant calendar">
+					<div className="list">
+					  <div className="row header">I DAG</div>
+					  <div className="group today"></div>
+					  <div className="row header">I MORGEN</div>
+					  <div className="group tomorrow"></div>
+					</div>
+				  </div>
+				</div>
+				<div className="quadrant news">
+					<NewsView { ...data.newsProps } />
+				</div>
 			</div>
-			<div className="quadrant news">
-				<NewsView { ...data.newsProps } />
+			<div className="ticker">
+				<div className="container">
+					<div className="header">
+						<span className="text">T-BANE</span>
+					</div>
+					<div className="entries">
+						<div className="entry">
+							<span className="line">5V</span>
+							<span className="time">3 MIN</span>
+						</div>
+						<div className="entry">
+							<span className="line">30Ø</span>
+							<span className="time">3 MIN</span>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
